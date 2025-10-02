56dfc72193f7880e0dba4ba4f33926d059df47c1
lukaakhalkatsi <luka.akhalkatsi.1@btu.edu.ge>
2025-10-03 00:57:58 +0400

    ref: remove redundant exercises

--- apps/exercises/urls.py
+++ apps/exercises/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ExerciseListView, ExerciseDetailView
 
 urlpatterns = [
-    path('exercises/', ExerciseListView.as_view(), name='exercise-list'),
-    path('exercises/<uuid:pk>/', ExerciseDetailView.as_view(), name='exercise-detail'),
+    path('', ExerciseListView.as_view(), name='exercise-list'),
+    path('<uuid:pk>/', ExerciseDetailView.as_view(), name='exercise-detail'),
 ]
