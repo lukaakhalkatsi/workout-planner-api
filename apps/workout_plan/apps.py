d07855324b6d52ba0c8b20ae9e527bba68431a40
lukaakhalkatsi <luka.akhalkatsi.1@btu.edu.ge>
2025-10-03 00:13:57 +0400

    chore: configure workout_plan and exercises app with correct AppConfig

--- apps/workout_plan/apps.py
+++ apps/workout_plan/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WorkoutPlanConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'workout_plan'
+    name = 'apps.workout_plan'
